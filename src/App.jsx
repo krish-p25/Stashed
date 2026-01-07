30a1c4f7d2de1ed51d2c89efe25dbb5b1cb5a52f
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-01-07 15:08:11 +0000

    added how it works page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Features from "./pages/Features.jsx";
+import HowItWorks from "./pages/HowItWorks.jsx";
 
 export default function App() {
     return (
@@ -8,6 +9,8 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/features" element={<Features />} />
+                <Route path="/how-it-works" element={<HowItWorks />} />
+
                 {/* Temporary catch-all (optional but recommended) */}
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
