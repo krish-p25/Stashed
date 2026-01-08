ba0001ab4f31875013546441f1e7ba259c54718a
Krish P <80683788+krish-p25@users.noreply.github.com>
2026-01-08 14:47:18 +0000

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Features from "./pages/Features.jsx";
 import HowItWorks from "./pages/HowItWorks.jsx";
 import UseCases from "./pages/UseCases.jsx";
 import Contact from "./pages/Contact.jsx";
+import Login from "./pages/Login";
 
 export default function App() {
     return (
@@ -14,6 +15,8 @@ export default function App() {
                 <Route path="/how-it-works" element={<HowItWorks />} />
                 <Route path="/use-cases" element={<UseCases />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="/login" element={<Login />} />
+
                 {/* Temporary catch-all (optional but recommended) */}
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
